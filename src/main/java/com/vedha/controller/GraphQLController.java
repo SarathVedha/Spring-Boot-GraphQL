8abd4d1367c1beae0c1851a4969580b6524c6ffe
Vedha <sarath1242000@gmail.com>
2024-06-16 22:31:47 +0530

    Spring Boot GraphQL

--- src/main/java/com/vedha/controller/GraphQLController.java
+++ src/main/java/com/vedha/controller/GraphQLController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
